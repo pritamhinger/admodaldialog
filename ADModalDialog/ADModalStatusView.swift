f91575d58b35dfb8d2612707f7d4054eff4188a5
pritam hinger <pritamgenius24@gmail.com>
2018-05-27 18:10:10 +0530

    Adding source file for custom dialog view.

---
+++ ADModalDialog/ADModalStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  ADModalStatusView.swift
+//  ADModalDialog
+//
+//  Created by Pritam Hinger on 27/05/18.
+//  Copyright © 2018 AppDevelapp. All rights reserved.
+//
+
+import UIKit
+
+class ADModalStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
