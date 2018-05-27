cbdbc538a7b9a06d34f0c416f6ae08173e20880b
pritam hinger <pritamgenius24@gmail.com>
2018-05-27 17:33:00 +0530

    Initial Commit

---
+++ ADModalDialog/ADModalDialog.h
@@ -0,0 +1,19 @@
+//
+//  ADModalDialog.h
+//  ADModalDialog
+//
+//  Created by Pritam Hinger on 27/05/18.
+//  Copyright © 2018 AppDevelapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADModalDialog.
+FOUNDATION_EXPORT double ADModalDialogVersionNumber;
+
+//! Project version string for ADModalDialog.
+FOUNDATION_EXPORT const unsigned char ADModalDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADModalDialog/PublicHeader.h>
+
+
